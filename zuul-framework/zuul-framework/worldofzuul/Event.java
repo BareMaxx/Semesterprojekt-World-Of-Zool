58babe583ea5ab2aeaae568f37ef6888b3f0a421
AGejr <albert.gejr@gmail.com>
2020-11-03 15:23:22 +0100

    Added runEvent method

--- zuul-framework/zuul-framework/worldofzuul/Event.java
+++ zuul-framework/zuul-framework/worldofzuul/Event.java
@@ -5,11 +5,18 @@ public class Event {
     private String name;
     private String description;
     private final int probabilityOfSuccess;   // (probability of the event being triggered)
+    private Player player;
 
-    Event(String name, int probabilityOfSuccess)
+    Event(String name, int probabilityOfSuccess, Player player)
     {
         this.name = name;
         this.probabilityOfSuccess = probabilityOfSuccess;
+        this.player = player;
+    }
+
+    public void runEvent()
+    {
+        
     }
 
     public String getName() {
