4b1131514be71792624b74ac854c576786e0764e
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2020-11-03 13:08:19 +0100

    Changed switch again lol, fix of extreamly long fucking line in CommandWord

--- zuul-framework/zuul-framework/worldofzuul/Child.java
+++ zuul-framework/zuul-framework/worldofzuul/Child.java
@@ -19,16 +19,13 @@ public class Child extends Game {
     public boolean processCommand(Command command){
         CommandWord commandWord = command.getCommandWord();
         switch(commandWord) {
-            case MONEY:
+            case MONEY -> {
                 System.out.println("You have " + super.getPlayer().getMoney() + " gold");
                 System.out.println("You are a child but will now grow to an adult to showcase how the code should work");
                 super.getPlayer().setStage("adult");
-                break;
-            case READ:
-                readBook();
-                break;
-            default:
-                super.processCommand(command);
+                }
+            case READ -> readBook();
+            default -> super.processCommand(command);
         }
         return true;
     }
