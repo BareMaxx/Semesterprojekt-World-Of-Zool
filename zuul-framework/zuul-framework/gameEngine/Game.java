3512ed019dd387340e8a9f101510839e35ba5cba
The Fern <krede2000@gmail.com>
2020-11-05 13:32:06 +0100

    Update Game.java

--- zuul-framework/zuul-framework/gameEngine/Game.java
+++ zuul-framework/zuul-framework/gameEngine/Game.java
@@ -87,8 +87,8 @@ public class Game {
                 System.out.println("You can't focus on anything in particular");
             } else {
                 System.out.println("You take a closer look at your surroundings\nYou notice:");
-                ArrayList<Item> items = getPlayer().currentRoom.items;
-                ArrayList<Item> objects = getPlayer().currentRoom.objects;
+                /*ArrayList<Item> items = getPlayer().getCurrentRoom().items;
+                ArrayList<Item> objects = getPlayer().getCurrentRoom().objects;
                 if (items.isEmpty() && objects.isEmpty()) {
                     System.out.println("\tnothing");
                 } else {
@@ -98,7 +98,7 @@ public class Game {
                     for (Item o : objects) {
                         System.out.println("\t" + o.getName());
                     }
-                }
+                }*/
             }
         }
     }
