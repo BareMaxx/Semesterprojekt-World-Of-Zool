330597aaacaf5b4e83fe1156b5a6dd8dc7505909
AGejr <albert.gejr@gmail.com>
2020-11-24 13:40:42 +0100

    Added fxml file, two images and a controller

---
+++ zuul-framework/zuul-framework/controller/outsideController.java
@@ -0,0 +1,65 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class outsideController {
+
+    @FXML
+    private ImageView backgroundImage;
+
+    @FXML
+    private ImageView roadSignImage;
+
+    @FXML
+    private Text homeText;
+
+    @FXML
+    private Text schooltext;
+
+    @FXML
+    private Text workText;
+
+    @FXML
+    private Text shopText;
+
+    @FXML
+    private Text hospitalText;
+
+    @FXML
+    void darkentext(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goHospital(ContextMenuEvent event) {
+
+    }
+
+    @FXML
+    void goSchool(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goShop(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goWork(MouseEvent event) {
+
+    }
+
+    @FXML
+    void highlightText(MouseEvent event) {
+
+    }
+
+}
