0f414653d8a733f8e711d8baf61ae0a534031413
AGejr <albert.gejr@gmail.com>
2020-11-24 14:50:19 +0100

    Added home controller and fxml file

--- zuul-framework/zuul-framework/main/Main.java
+++ zuul-framework/zuul-framework/main/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/outside.fxml"));
-        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setTitle("Outside");
         primaryStage.setScene(new Scene(root, 1280 , 720));
         primaryStage.show();
     }
