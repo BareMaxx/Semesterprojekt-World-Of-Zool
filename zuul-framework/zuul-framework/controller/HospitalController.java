a8071b355a502ee29525af09289ff080347b0443
The Fern <krede2000@gmail.com>
2020-11-26 12:43:12 +0100

    Added hospital FXML and controller

---
+++ zuul-framework/zuul-framework/controller/HospitalController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class HospitalController extends GenericController {
+    public HospitalController(){
+        super();
+    }
+
+    @FXML
+    void heal(MouseEvent event) {
+
+    }
+}
