5461f2543db6cceafef6b18a79aed1253b78677b
Max Hansen (Maxx0758) <maxx0758@gmail.com>
2020-11-26 13:10:09 +0100

    Remove very old TODO comments

--- zuul-framework/zuul-framework/gameEngine/Old.java
+++ zuul-framework/zuul-framework/gameEngine/Old.java
@@ -5,7 +5,6 @@ import player.Player;
 import commands.Command;
 import commands.CommandWord;
 
-//TODO: work method -> inc money
 public class Old extends Game {
     public Old(Player p1, Parser parser) {
         super(p1, parser, 20);
